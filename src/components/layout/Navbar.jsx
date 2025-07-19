aa72a86980c6f693e8532c946963b2d31d881c5f
dahyuniiiiii <imcute0703123@naver.com>
2025-07-20 02:05:19 +0900

    Feat - 로그인/로그아웃 기능 구현

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 import "../../css/Navbar.css";
 import LoginModal from "../modal/LoginModal";
 import navHamburgerLogo from "../../assets/images/navHamburgerLogo.png";
-import navBellIcon from "../../assets/images/navBellIcon.png";
+import navBellIconOn from "../../assets/images/navBellIconOn.png"; 
+import navBellIconOff from "../../assets/images/navBellIconOff.png";
 
 function Navbar({ isLoggedIn }) {
   const [isLoginModal, setIsLoginModal] = useState(false);
-
+  const [isBellOn, setIsBellOn] = useState(false);
+  const handleBellClick = () => {
+    setIsBellOn((prev) => !prev);
+  };
   return (
     <div className="navbar">
       <img src={navHamburgerLogo} />
       {!isLoggedIn ? (
         <div onClick={() => setIsLoginModal(true)}>로그인</div>
       ) : (
-        <img src={navBellIcon} />
+        <img
+          src={isBellOn ? navBellIconOn : navBellIconOff}
+          onClick={handleBellClick}
+        />
       )}
       {isLoginModal && <LoginModal onClose={() => setIsLoginModal(false)} />}
     </div>
